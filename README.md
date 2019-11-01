80bba7c4b6ff606146a2773b750ce506860d823b
Anton Krylov <anthony.kryloff@gmail.com>
2019-11-01 23:22:44 +0300

    update README, add some comments and examples

--- README.md
+++ README.md
@@ -10,3 +10,15 @@ Supported inputs:
 Supported outputs:
 
 * Telegram channels and private chats (works as a bot)
+
+## How to run
+
+1. Create a Standalone VK application [here](https://vk.com/editapp?act=create)
+and get your app id and Service key
+2. Create a Telegram bot with @BotFather
+3. Setup your `.env` file as per [template](./.env.template), 
+or put everything into environment variables
+4. Run
+```shell script
+python3 crawler.py
+```
--- settings.py
+++ settings.py
@@ -1,10 +1,12 @@
 import os
 
 from dotenv import load_dotenv
+
+# Load properties into os.environ from .env file, if present
 load_dotenv()
 
-APP_ID = int(os.getenv('APP_ID'))
-APP_TOKEN = os.getenv('APP_TOKEN')
+APP_ID = int(os.getenv('APP_ID'))  # VK standalone app id
+APP_TOKEN = os.getenv('APP_TOKEN')  # A service token is enough
 
-BOT_TOKEN = os.getenv('BOT_TOKEN')
-BOT_CHAT_ID = os.getenv('BOT_CHAT_ID')
+BOT_TOKEN = os.getenv('BOT_TOKEN')  # Bot token from the BotFather
+BOT_CHAT_ID = os.getenv('BOT_CHAT_ID')  # Either chat id or a @channel-name, bot should have write access to it
