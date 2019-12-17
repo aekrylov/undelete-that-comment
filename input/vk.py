c7d3075e972ae05de641411bf1a90ab2b1248d37
Anton Krylov <krylov@cg.ru>
2019-12-17 12:31:20 +0300

    Revert "fix(vk): latest API changes"
    This reverts commit 60e90dc7

--- input/vk.py
+++ input/vk.py
@@ -47,12 +47,7 @@ class VkDiscussionSource(VkSourceBase):
             print(e)
             resp = e.try_method()
 
-        items = resp['items']
-        if items[0] is int:
-            # First item appears to be some id
-            items = items[1:]
-
-        last_comments = [self._to_object(comment) for comment in items]
+        last_comments = [self._to_object(comment) for comment in resp['items']]
 
         present_map = {c.comment_id: c for c in last_comments}
         m.update(present_map)
